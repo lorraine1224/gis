5cf5ac9d899bacf2f48d4bc22a5beebba343486a
lorraine <1572823749@qq.com>
2021-09-01 16:37:47 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <GIS/>
+  </div>
+</template>
+<script>
+import GIS from './views/gis.vue'
+export default {
+  components:{GIS}
+}
+</script>
+<style>
+*{
+  padding: 0;
+  margin: 0;
+}
+</style>
